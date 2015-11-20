5245a6b547d1b769618b758f9eca765dda896438
aneesh17 <aneesh.ravishankar@gmail.com>
2015-11-20 23:38:48 +0530

    Submit Commit

--- src/sortedArrayInsertNumber.cpp
+++ src/sortedArrayInsertNumber.cpp
@@ -13,18 +13,53 @@ NOTES: Use realloc to allocate memory.
 
 #include <stdio.h>
 #include <malloc.h>
-
+int * insertAtPosition(int*, int, int, int);
 int * sortedArrayInsertNumber(int *Arr, int len, int num)
 {
-	int index, check, flag;
-	int hi = 0, lo = len - 1;
-/*	while (1)
+	if (Arr == NULL || len < 0)		
+		return NULL;
+	int index;
+	int hi = len - 1, lo = 0;
+	if (num < Arr[0])
+	{
+		Arr = insertAtPosition(Arr, len+1, num, 0);
+		return Arr;
+	}
+	if (num > Arr[len - 1])
+	{
+		Arr = insertAtPosition(Arr, len + 1, num, len);
+		return Arr;
+	}
+	while (1)
 	{
 		index = (hi + lo) / 2;
-		if (Arr[index] == num)
+		if (Arr[index] > num)		
+			hi = index;		
+		else if (Arr[index] < num)
+			lo = index;
+		else
 		{
-
+			Arr = insertAtPosition(Arr, len + 1, num, index);	
+			break;
 		}
-	}*/
-	return NULL;
+		
+		if (lo == (hi - 1))
+		{
+			Arr = insertAtPosition(Arr, (len+1), num, hi);			
+			break;
+		}
+	}//while	
+	return Arr;
+}
+
+int * insertAtPosition(int *Arr, int len, int num, int index)
+{
+	int indexForArr;
+	Arr = (int *)realloc(Arr, len*sizeof(int));
+	for (indexForArr = len - 2; indexForArr >= index; indexForArr--)
+	{
+		Arr[indexForArr + 1] = Arr[indexForArr];
+	}
+	Arr[indexForArr + 1] = num;
+	return Arr;
 }
