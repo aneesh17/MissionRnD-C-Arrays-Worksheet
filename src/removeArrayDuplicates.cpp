1ee92a14501c2f9efe0eb787ab24b3082848d214
aneesh17 <aneesh.ravishankar@gmail.com>
2015-11-22 22:52:25 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -12,8 +12,60 @@ NOTES: Don't create new array, try to change the input array.
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
+void Merge(int*, int, int);
+void sort(int*, int, int, int);
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	return NULL;
+	if (Arr == NULL || len < 0)
+		return NULL;	
+	Merge(Arr, 0, (len-1));
+	return Arr;
+}
+
+void Merge(int *Arr, int lo, int hi)
+{
+	int mid;
+	if (lo < hi)
+	{
+		mid = (lo + hi) / 2;
+		Merge(Arr, lo, mid);
+		Merge(Arr, (mid + 1), hi);
+		sort(Arr, lo, mid, hi);
+	}
+}
+
+void sort(int *Arr, int lo, int mid, int hi)
+{
+	int size1, size2;
+	int i = 0, j = 0, k = 0;
+	size1 = mid - lo + 1;
+	size2 = hi - mid;
+	int *arr1 = (int*)malloc(size1*sizeof(int));
+	int *arr2 = (int*)malloc(size2*sizeof(int));
+	for (i = 0; i < size1; i++)
+	{
+		arr1[i] = Arr[lo + i];
+	}
+	arr1[i] = 99999;
+	for (i = 0; i < size2; i++)
+	{
+		arr2[i] = Arr[mid + 1 + i];
+	}
+	arr2[i] = 99999;
+	i = 0;
+	j = 0;
+	for (k = lo; k <= hi; k++)
+	{
+		if (arr1[i] < arr2[j])
+			Arr[k] = arr1[i++];
+		else if (arr1[i] > arr2[j])
+			Arr[k] = arr2[j++];
+		else
+		{
+			Arr[k] = arr1[i++];		
+			j++;
+		}
+	}
+	
 }
